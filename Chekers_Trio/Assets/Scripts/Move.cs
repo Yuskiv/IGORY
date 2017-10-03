84159867e55ee3e959e83c5f73e116e1f994ff62
Yuskiv <pavloyuskiv@gmail.com>
2017-10-03 23:16:45 +0300

    1 commit

---
+++ Chekers_Trio/Assets/Scripts/Move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour{
+
+    float x;
+    float y;
+    float z = 1;
+
+    void Update()
+    {
+        x = Input.mousePosition.x;
+        y = Input.mousePosition.y;
+        z = 1;
+    }
+
+    void OnMouseDrag()
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(x, y, z));
+    }
+}
